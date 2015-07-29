76348cf832c8546979573b615cffe9ace0d8b5c8
Haotong Chen <hereischen@gmail.com>
2015-07-29 15:42:14 +0800

    bug fixing

--- setup.py
+++ setup.py
@@ -1,24 +1,20 @@
-
 import os
 from setuptools import setup
 
 with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
     README = readme.read()
 
-with open(os.path.join(os.path.dirname(__file__), 'CHANGES.rst')) as changes:
-    CHANGES = changes.read()
-
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-luoji-accounts',
-    version='0.0.1',
+    version='0.0.3',
     packages=['accounts'],
     include_package_data=True,
     license='BSD License',
     description='A general account app for projects.',
-    long_description=README + '\n\n' + CHANGES,
+    long_description=README,
     url='https://github.com/hereischen/django-luoji-accounts',
     author='Haotong Chen',
     author_email='hereischen@gmail.com',
