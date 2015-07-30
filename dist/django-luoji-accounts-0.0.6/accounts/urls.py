74c2deae195c01b77dddbdd08dec80f2450c26d8
Haotong Chen <chenhaotong@chenhaotongdeMacBook-Pro.local>
2015-07-30 15:57:38 +0800

    add gitignore

--- dist/django-luoji-accounts-0.0.6/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.conf.urls import patterns, url
-from .api import transfer_detail
-
-
-urlpatterns = patterns(
-    'accounts.views',
-    url(r'^api/transfer_detail/(?P<user_id>\d+)/', transfer_detail),
-)
